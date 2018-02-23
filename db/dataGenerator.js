cc0b53913131e3e8fbafdebf37b2f14b70a66ad3
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-22 16:31:30 -0800

    create data generation function

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -1,19 +1,108 @@
-var faker = require('faker');
+const faker = require('faker');
 
-// console.log(faker.name.findName());
-// console.log(faker.internet.email());
-// console.dir(faker.helpers.createCard());
+// get a random number between min(inclusive) and max(non-inclusive).
+// [decimalPlaces]: optional. default to 0(integer) if not provided
+const getRandomNum = (min, max, decimalPlaces) => {
+  decimalPlaces = decimalPlaces || 0;
+  let multiplier = Math.pow(10, decimalPlaces);
+  let minAdj = min * multiplier;
+  let maxAdj = ((max - 1) * multiplier) + 1;
 
-var obj = {
-  id: 1,
-  url: `/listings/1`,
-  title: faker.lorem.words(5),
-  type: 'Entire Apartment',
-  numBeds: 3,
-  price: 204,
-  numRatings: 287,
-  avgStars: 3.4,
-  coverImage: `https://picsum.photos/316/210?image=${1}`,
+  let randomNumAdj = Math.floor(Math.random() * (maxAdj - minAdj)) + minAdj
+  return randomNumAdj / multiplier;
+}
+
+// pick a random item from an array
+const pickRandomItem = array => {
+  return array[getRandomNum(0, array.length)];
+}
+
+const settings = {
+  id: {
+    min: 0,
+    max: 200, // non-inclusive
+  },
+
+  title: {
+    minWords: 2,
+    maxWords: 8, // non-inclusive
+  },
+
+  type: {
+    options: ['Entire Place', 'Shared Room', 'Private Room'],
+  },
+
+  numBeds: {
+    min: 1,
+    max: 20, // non-inclusive
+  },
+
+  price: {
+    min: 10,
+    max: 2000, // non-inclusive
+  },
+
+  numRatings: {
+    min: 0,
+    max: 2000, // non-inclusive
+  },
+
+  avgStars: {
+    min: 0,
+    max: 6, // non-inclusive
+    decimalPlaces: 1,
+  },
+
+  thumbnailImage: {
+    width: 316,
+    height: 210,
+  },
 };
 
-console.log(JSON.stringify(obj));
+const generateListings = (settings) => {
+  let array = [];
+  for (let id = settings.id.min; id < settings.id.max; id++) {
+    let document = {
+      id: id,
+
+      url: `/listings/${id}`,
+
+      title: faker.lorem.words(getRandomNum(
+        settings.title.minWords,
+        settings.title.maxWords
+      )),
+
+      type: pickRandomItem(
+        settings.type.options
+      ),
+
+      numBeds: getRandomNum(
+        settings.numBeds.min,
+        settings.numBeds.max
+      ),
+
+      price: getRandomNum(
+        settings.price.min,
+        settings.price.max
+      ),
+
+      numRatings: getRandomNum(
+        settings.numRatings.min,
+        settings.numRatings.max
+      ),
+
+      avgStars: getRandomNum(
+        settings.avgStars.min,
+        settings.avgStars.max,
+        settings.avgStars.decimalPlaces
+      ),
+
+      thumbnailImage: `https://picsum.photos/` +
+                      `${settings.thumbnailImage.width}/` +
+                      `${settings.thumbnailImage.height}` +
+                      `?image=${id}`,
+    };
+    array.push(document);
+  }
+  return array;
+};
