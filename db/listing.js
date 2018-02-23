4e45a11093c8fbf8eafb1d6c2210d42d671b3da2
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-23 15:05:57 -0800

    seeded database

--- db/listing.js
+++ db/listing.js
@@ -1,23 +1,35 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/seabnb');
 
-var db = mongoose.connection;
+const similarListingSchema = mongoose.Schema({
+  id: Number,
+  url: String,
+  title: String,
+  type: String,
+  numBeds: Number,
+  price: Number,
+  numRatings: Number,
+  avgStars: Number,
+  thumbnailImage: String,
+})
 
-var listingSchema = mongoose.Schema({
+const listingSchema = mongoose.Schema({
   id: {
     type: Number,
     unique: true,
   },
-
-  similarListings: [{
-    id: Number,
-    url: String,
-    title: String,
-    type: String,
-    numBeds: Number,
-    price: Number,
-    numRatings: Number,
-    avgStars: Number,
-    thumbnailImage: String,
-  }],
+  similarListings: [similarListingSchema],
 });
+
+const ListingModel = mongoose.model('Listing', listingSchema);
+
+module.exports = ListingModel;
+
+
+// mongoose.connect('mongodb://localhost/seabnb');
+
+// var db = mongoose.connection;
+
+// db.on('error', error => console.log('connection error:', error));
+// db.once('open', () => {
+
+// })
