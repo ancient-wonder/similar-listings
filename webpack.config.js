02c618459bb75d5638a5604ab7f8ce1e93bc2d97
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-26 12:03:34 -0800

    configure webpack i/o dirs and babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+
+// Build directory is where the bundle file will be placed
+const BUILD_DIR = path.resolve(__dirname, 'client/dist');
+// App directory is where raw JSX files will be placed
+const APP_DIR = path.resolve(__dirname, 'client/src');
+
+const config = {
+  entry: APP_DIR + '/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: APP_DIR,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+};
+
+module.exports = config;
