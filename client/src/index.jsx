d9a328eed7a2dc4921e5ef35a9a1b474ceead04b
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-26 15:41:50 -0800

    install jest and create basic test file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,14 @@ import ReactDom from 'react-dom';
 
 const title = 'Minimal React Webpack Babel Setup';
 
-ReactDom.render(
-  <div>{title}</div>,
-  document.getElementById('app')
-);
+// ReactDom.render(
+//   <div>{title}</div>,
+//   document.getElementById('app')
+// );
+
+const sum = function(a, b) {
+  console.log(a);
+  return a + b;
+}
+
+export default sum;
