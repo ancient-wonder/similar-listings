9cebbc82e550e74c42bda6c71fb816db385a54ac
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-26 22:35:23 -0800

    cleanup in React app index file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,14 +3,7 @@ import ReactDom from 'react-dom';
 
 const title = 'Minimal React Webpack Babel Setup';
 
-// ReactDom.render(
-//   <div>{title}</div>,
-//   document.getElementById('app')
-// );
-
-const sum = function(a, b) {
-  console.log(a);
-  return a + b;
-}
-
-export default sum;
+ReactDom.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
