f6b2542446454740aada92f8c268544a074d56f6
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-27 14:18:46 -0800

    build internal Listing component structure

--- client/src/listing.jsx
+++ client/src/listing.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Listing = (props) => (
-  <div>this is a listing</div>
-);
+const Listing = ({info}) => {
+  const {
+    id,
+    url,
+    title,
+    type,
+    numBeds,
+    price,
+    numRatings,
+    avgStars,
+    thumbnailImage,
+  } = info;
+
+  const additionalDetails = {
+    title,
+    type,
+    numBeds,
+    price,
+    numRatings,
+    avgStars
+  };
+
+  return (
+  <div className="similar-listing">
+    <img src={thumbnailImage} />
+    <div className="similar-listing-details">
+      put Details component here<br />
+      {JSON.stringify(additionalDetails)}<br />
+    </div>
+  </div>
+  )
+};
 
 Listing.propTypes = {
   info: PropTypes.object.isRequired,
