245da23d6f895bb30ade7d54b66a0f2192bd0739
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-27 13:15:23 -0800

    create SimilarListings skeleton with header and three listings

--- client/src/similarListings.jsx
+++ client/src/similarListings.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Listing from './listing';
 
 class SimilarListings extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      listingsShown: props.listings.slice(0, 3), // MVP: only show 3 listings
+    };
   }
 
   render() {
     return (
-      <div testProp={this.props.testProp}>This is the Similar Listings component</div>
+      <div className="similar-listings">
+        <h1>Similar Listings</h1>
+        <div className="similar-listings-ribbon">
+          {this.state.listingsShown.map(listing => (
+            <div>this represents a single listing</div>
+          ))}
+        </div>
+      </div>
     );
   }
 }
 
 SimilarListings.propTypes = {
-  testProp: PropTypes.string.isRequired,
+  listings: PropTypes.array.isRequired,
 };
 
 export default SimilarListings;
