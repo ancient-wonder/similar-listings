9bd4debb029e1563c412057c077dd13763f72373
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-27 12:41:41 -0800

    create dummy data file to use while building client

---
+++ client/dummyData.js
@@ -0,0 +1,112 @@
+export default [
+  {
+    "id":199,
+    "url":"/listings/199",
+    "title":"nostrum laboriosam in rerum officiis",
+    "type":"Shared Room",
+    "numBeds":2,
+    "price":810,
+    "numRatings":965,
+    "avgStars":0.5,
+    "thumbnailImage":"https://picsum.photos/316/210?image=199"
+  },
+  {
+    "id":180,
+    "url":"/listings/180",
+    "title":"sint itaque voluptatem eligendi",
+    "type":"Entire Place",
+    "numBeds":1,
+    "price":609,
+    "numRatings":697,
+    "avgStars":1,
+    "thumbnailImage":"https://picsum.photos/316/210?image=180"
+  },
+  {
+    "id":79,
+    "url":"/listings/79",
+    "title":"voluptatibus fugit saepe ipsa expedita rem",
+    "type":"Entire Place",
+    "numBeds":16,
+    "price":664,
+    "numRatings":48,
+    "avgStars":2.4,
+    "thumbnailImage":"https://picsum.photos/316/210?image=79"
+  },
+  {
+    "id":161,
+    "url":"/listings/161",
+    "title":"maiores iusto animi aut nobis",
+    "type":"Entire Place",
+    "numBeds":5,
+    "price":1452,
+    "numRatings":1718,
+    "avgStars":4.8,
+    "thumbnailImage":"https://picsum.photos/316/210?image=161"
+  },
+  {
+    "id":151,
+    "url":"/listings/151",
+    "title":"sed soluta laudantium alias et",
+    "type":"Entire Place",
+    "numBeds":9,
+    "price":158,
+    "numRatings":1194,
+    "avgStars":1.4,
+    "thumbnailImage":"https://picsum.photos/316/210?image=151"
+  },
+  {
+    "id":40,
+    "url":"/listings/40",
+    "title":"reiciendis et aliquid harum",
+    "type":"Shared Room",
+    "numBeds":4,
+    "price":1025,
+    "numRatings":332,
+    "avgStars":3.9,
+    "thumbnailImage":"https://picsum.photos/316/210?image=40"
+  },
+  {
+    "id":35,
+    "url":"/listings/35",
+    "title":"impedit exercitationem sed",
+    "type":"Entire Place",
+    "numBeds":15,
+    "price":1801,
+    "numRatings":999,
+    "avgStars":2.7,
+    "thumbnailImage":"https://picsum.photos/316/210?image=35"
+  },
+  {
+    "id":107,
+    "url":"/listings/107",
+    "title":"magni sapiente a facere asperiores",
+    "type":"Private Room",
+    "numBeds":16,
+    "price":65,
+    "numRatings":372,
+    "avgStars":4.1,
+    "thumbnailImage":"https://picsum.photos/316/210?image=107"
+  },
+  {
+    "id":164,
+    "url":"/listings/164",
+    "title":"porro ipsam quam distinctio quo quo",
+    "type":"Private Room",
+    "numBeds":15,
+    "price":414,
+    "numRatings":695,
+    "avgStars":2.4,
+    "thumbnailImage":"https://picsum.photos/316/210?image=164"
+  },
+  {
+    "id":175,
+    "url":"/listings/175",
+    "title":"consequuntur earum in accusantium",
+    "type":"Entire Place",
+    "numBeds":4,
+    "price":1429,
+    "numRatings":1399,
+    "avgStars":3.5,
+    "thumbnailImage":"https://picsum.photos/316/210?image=175"
+  },
+];
