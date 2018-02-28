8d87f2920d1f032a3cf07f43ec0b6c5fc8a8232d
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-28 14:44:25 -0800

    add CSS reset and initial basic style using Sass

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SimilarListings from './similarListings';
+import './styles/app.scss';
 
 // in real life we'll want to make a GET request to the server
 // to get the listings data
