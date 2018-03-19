13d6a10fdea59dfd2650250cacefd3e397c1ecdf
Michael <openmichael65@gmail.com>
2018-03-19 16:54:46 -0700

    restructured postgres code to seperate schema into different files

---
+++ db/postgres/SQLscript/createTB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE list(
+  id INTEGER PRIMARY KEY,
+  url TEXT,
+  title TEXT,
+  type TEXT,
+  numbeds INTEGER,
+  price INTEGER,
+  numratings DOUBLE PRECISION,
+  avgstars DOUBLE PRECISION,
+  thumbnailimage TEXT
+);
---
+++ db/postgres/SQLscript/postgres.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const QueryFile = require('pg-promise').QueryFile;
+
+const sql = (file) => {
+  const fullpath = path.join(__dirname, file);
+
+  const options = { minify: true };
+
+  const qf = new QueryFile(fullpath, options);
+
+  return qf;
+};
+
+module.exports = {
+  dropDB: sql('./dropDB.sql'),
+  createDB: sql('./createDB.sql'),
+  createTB: sql('./createTB.sql'),
+};
